e145ccf61c310df8bd51a5f3a2b714e2f49957cf
ab3llini <albertomario.bellini@mail.polimi.it>
2018-01-30 17:46:43 +0100

    Added possibility to switch Host (CPU/GPU) for LP formulations

--- source/lpsolver/simplexpd.py
+++ source/lpsolver/simplexpd.py
@@ -10,19 +10,30 @@ class LPException(Exception):
     """TODO"""
 
 
+class LPHost(Enum):
+    CPU = 1
+    GPU = 2
+
+
 class LPObjective(Enum):
     MINIMIZE = "min"
     MAXIMIZE = "max"
 
 
+# Utility to dynamically create representation of matrix for cpu using numpy or gpu using cudamat
+class LPMatrixBuilder:
+
+    def __init__(self, host=LPHost.CPU):
+        self.host = host;
+
+    def build(self, matrix):
+        m = np.array(matrix)
+        return m if self.host == LPHost.CPU else cm.CUDAMatrix(m)
+
+
 class LPProblem:
 
-    def __init__(self, objective, cost_v, constraints_v, constant_v, var_names="x", cuda_input=False):
-
-        if not cuda_input:
-            cost_v = cm.CUDAMatrix(np.array(cost_v))
-            constant_v = cm.CUDAMatrix(np.array(constant_v))
-            constraints_v = cm.CUDAMatrix(np.array(constraints_v))
+    def __init__(self, objective, cost_v, constraints_v, constant_v, var_names="x"):
 
         if cost_v.shape[ROW] > 1:
             raise Exception("Incompatible cost vector: cost vector has %s rows instead of 1" % (cost_v.shape[ROW]))
@@ -48,26 +59,31 @@ class LPProblem:
             self.b.transpose(),
             self.A.transpose(),
             self.c.transpose(),
-            var_name,
-            True
+            var_name
         )
 
     def __str__(self):
         x = "%s {" % self.obj.value
-        for i, e in enumerate((self.c.asarray())[0]):
-            x += " %s%.2f * %s%d" % (("+ " if e >= 0 and i != 0 else ""), e, self.var_names, i+1)
+        for i, e in enumerate((self.c.asarray() if type(self.c) is not np.ndarray else self.c)[0]):
+            x += " %s%.2f * %s%d" % (("+ " if e >= 0 and i != 0 else ""), e, self.var_names, i + 1)
         x += " }\n"
 
-        for i, row in enumerate(self.A.asarray()):
+        for i, row in enumerate(self.A.asarray() if type(self.A) is not np.ndarray else self.A):
             for j, col_e in enumerate(row):
                 x += "%s%.2f * %s%d " % (("+ " if col_e >= 0 and j != 0 else ""), col_e, self.var_names, j + 1)
-            x += "= %d\n" % self.b.asarray()[i]
+            x += "= %d\n" % (self.b.asarray() if type(self.b) is not np.ndarray else self.b)[i]
         return x
 
 
 c = [[2, 3]]
 A = [[4, 5], [6, 8], [3, 5]]
 b = [[5], [7], [4]]
+
+builder = LPMatrixBuilder(LPHost.CPU)
+
+c = builder.build(c)
+A = builder.build(A)
+b = builder.build(b)
 
 try:
     p = LPProblem(LPObjective.MAXIMIZE, c, A, b)
