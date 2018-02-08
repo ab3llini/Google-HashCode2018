67ed0d34489ef161980f6962473682b8bc769059
Luca <cavalli1234@hotmail.it>
2018-02-08 15:45:29 +0100

    matrix read utility added

--- source/text_parser/parser.py
+++ source/text_parser/parser.py
@@ -1,5 +1,6 @@
 import os
 import re
+import numpy as np
 
 
 def _robust_str_int_cast(in_str):
@@ -45,7 +46,7 @@ class Parser:
     def read_line(self, reducer=lambda *x: x, splitter=_space_splitter, mapper=_robust_str_int_cast):
         """
         Perform a single line map-reduce parsing:
-        1) the next line is read from the file (until a \n is found. \n is INCLUDED)
+        1) the next line is read from the file (until a \n is found. \n is NOT included)
         2) the line is separated into tokens, based on the splitter function.
         3) the tokens are mapped according to the mapper function
         4) the list of mapped tokens is reduced through the reducer. Tokens are passed as varargs
@@ -54,8 +55,29 @@ class Parser:
         :param mapper: the function to map string tokens to useful elements. A robust cast to int by default
         :return: the result of the reduction of step 4, whatever it is.
         """
-        tokens = splitter(self.__in.readline())
+        tokens = splitter(self.__in.readline()[:-1])
         return reducer(*_map(tokens, mapper))
+
+    def read_matrix(self, rows, cols, column_splitter=_space_splitter, mapper=_robust_str_int_cast):
+        """
+        Read multiple lines in order to directly extract a matrix of tokens
+        :param rows: expected rows of the matrix.
+        :param cols: expected cols of the matrix.
+        :param column_splitter: splitter used to separate a row in the file
+                        into its component tokens. Space-split by default
+        :param mapper: the function to map string tokens to useful elements.
+                        A robust cast to int by default
+        :return: a numpy matrix containing rows * cols tokens read from the input file
+        """
+        mat = []
+        warned = False
+        for _ in range(rows):
+            row = self.read_line(reducer=lambda *x: list(x), mapper=mapper, splitter=column_splitter)
+            if not warned and len(row) != cols:
+                print("Warning: row with unexpected number of tokens has been found")
+                warned = True
+            mat.append(row)
+        return np.array(mat)
 
     def close(self):
         """
@@ -97,6 +119,10 @@ class ATestClass:
         return "I am the test class.\nproduct: " + str(self.prod) + "\nsum: " + str(self.sum)
 
 
+# Test basic matrix reading:
+print(p.read_matrix(rows=3, cols=3))
+# Test custom-splitter matrix reading:
+print(p.read_matrix(rows=4, cols=3, column_splitter=lambda s: list(s), mapper=lambda t: t))
 # Test a custom robust reducer
 print(p.read_line(reducer=reduce_sum))
 # Test the default reducer
