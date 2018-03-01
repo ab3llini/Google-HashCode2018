6386059b5d7b8d16d08d6dea7eb7e077c87104eb
Luca <cavalli1234@hotmail.it>
2018-03-01 21:21:44 +0100

    c

--- source/hash2018/greedyL.py
+++ source/hash2018/greedyL.py
@@ -4,17 +4,19 @@ from hash2018.reader import *
 from hash2018.writer import *
 
 
-inst = EXAMPLE
+inst = NOHURRY
 
 if __name__ == '__main__':
     problem = read_in(inst)
-    avrides = problem[DATA]
+    avrides = problem[DATA][:]
     chrides = []
+    pts = 0
     for _ in range(problem[FLEET]):
         ch, av = schedule_car((0, 0), 0, avrides, problem[BONUS])
         chrides.append(ch)
         avrides = av
+        pts += get_end_stats([problem[DATA][i] for i in chrides[-1]], problem[BONUS])[2]
         if len(avrides) == 0:
             break
-
-    write_sol(inst, "greedyL", chrides)
+    print(pts)
+    write_sol(inst, "greedyL%d" % pts, chrides)
--- source/hash2018/writer.py
+++ source/hash2018/writer.py
@@ -4,11 +4,8 @@ from text_parser.writer import *
 
 def write_sol(id, name, sol):
     path = get_sol(id, name)
-    print(sol)
     with TextWriter(path) as tw:
         for ride in sol:
-            # print(ride)
             out = [len(ride)]
             out.extend(ride)
-            # print(out)
             tw.write_line(out)
