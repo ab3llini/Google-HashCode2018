a2e0145e402314e49cef6806e2fdeb54ced3d719
ab3llini <albertomario.bellini@mail.polimi.it>
2018-03-01 19:56:10 +0100

    Line up

--- source/simplesolution.py
+++ source/simplesolution.py
@@ -18,15 +18,18 @@ class Car:
     def __str__(self):
         return "x=%d, y=%s" % (self.x, self.y)
 
-    def utility(self, ride, time):
-        distance = np.abs(ride[1][0] - self.x) + np.abs(ride[1][1] - self.y)
+    def utility(self, ride, now):
 
+        timetoreach = np.abs(ride[1][0] - self.x) + np.abs(ride[1][1] - self.y)
         ridestart = ride[3][0]
 
-        if (ridestart - time) < distance:
+        if (ridestart - now) < timetoreach:
             return 0
         else:
             gain = np.abs(ride[2][0] - ride[1][0]) + np.abs(ride[1][1] - ride[1][1])
+            timetowait = ride[3][0] - timetoreach - now
+            timetotravel = gain
+            return gain / (timetoreach + timetowait + timetotravel)
 
 
 cars = [Car()] * data[FLEET]
