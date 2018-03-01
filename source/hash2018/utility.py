94820731cc841190d3d99f264576ef530e7b7013
ab3llini <albertomario.bellini@mail.polimi.it>
2018-03-01 20:39:13 +0100

    Line up

--- source/hash2018/utility.py
+++ source/hash2018/utility.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def manhattan_dist(p1, p2):
-    np.abs(p1[0] - p2[0]) + np.abs(p1[1] - p2[1])
+    return np.abs(p1[0] - p2[0]) + np.abs(p1[1] - p2[1])
 
 def start_time(ride):
     return ride[3][0]
