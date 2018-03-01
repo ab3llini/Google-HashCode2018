f1846c4ef1b6968cb41994b0769f89db91fc18cb
Luca <cavalli1234@hotmail.it>
2018-03-01 18:43:17 +0100

    read/write extended prorotype

--- source/hash2018/reader.py
+++ source/hash2018/reader.py
@@ -1,7 +1,22 @@
 from text_parser.conventions import *
 from text_parser.parser import *
 
+
 def read_in(id):
     path = get_in(id)
     with Parser(path) as p:
         pass
+
+
+def read_sol(id, name):
+    path = get_sol(id, name)
+    with Parser(path) as p:
+        pass
+
+
+def read_all_solutions(id):
+    names = get_sol_names_list(id)
+    ret = []
+    for n in names:
+        ret.append(read_sol(id, n))
+    return ret
--- source/hash2018/writer.py
+++ source/hash2018/writer.py
@@ -1,2 +1,9 @@
 from text_parser.conventions import *
-from text_parser.parser import *
+from text_parser.writer import *
+
+
+def write_sol(id, name):
+    path = get_sol(id, name)
+    with TextWriter(path) as tw:
+        pass
+
