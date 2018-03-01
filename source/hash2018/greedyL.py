d8c4a7ece91bd74c9f8aa6ffa2790c50e9ae70b0
Luca <cavalli1234@hotmail.it>
2018-03-01 22:02:43 +0100

    c

--- source/hash2018/greedyL.py
+++ source/hash2018/greedyL.py
@@ -30,5 +30,24 @@ if __name__ == '__main__':
         if len(avrides) == 0:
             break
         print("%d/%d" % (index, problem[FLEET]))
+    while True:
+        for index in range(problem[FLEET]):
+            if ended[index]:
+                continue
+            sts = get_end_stats(chrides[index], problem[BONUS])
+            ch, av = schedule_car(sts[0], sts[1], avrides, problem[BONUS], nottakingbonus[index])
+            if len(ch) == 0:
+                if nottakingbonus[index]:
+                    ended[index] = True
+                else:
+                    nottakingbonus[index] = True
+                if all(ended):
+                    break
+            chrides[index].extend(ch)
+            avrides = av
+            # pts += get_end_stats([problem[DATA][i] for i in chrides[-1]], problem[BONUS])[2]
+            if len(avrides) == 0:
+                break
+            print("%d/%d" % (index, problem[FLEET]))
     # print(pts)
     write_sol(inst, "greedyL", chrides)
